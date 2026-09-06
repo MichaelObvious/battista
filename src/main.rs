7e87add24d06b9f3f00d3ac99e94e9db3870a3dc
MichaelObvious <michele.palese42@outlook.it>
2026-09-06 16:33:58 +0200

    idk

--- src/main.rs
+++ src/main.rs
@@ -986,7 +986,7 @@ fn write_typ_report(file_path: &PathBuf, stats: &StatsCollection, budget: &Budge
     let mut slices  = Vec::new();
 
     for (category, monthly_budget) in budget_categories.iter() {
-        if *monthly_budget > dec!(0) && (monthly_budget / monthly_total_budget) * dec!(100) >= dec!(2) {
+        if *monthly_budget > dec!(0) && (monthly_budget / monthly_total_budget) * dec!(100) >= dec!(1) {
             total_allocated += monthly_budget;
             slices.push((category.to_owned().to_owned(), *monthly_budget));
         }
